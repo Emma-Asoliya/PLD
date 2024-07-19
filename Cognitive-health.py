e26fcf70b951d070759bd22286b19c4c2769e02f
Emma-Asoliya <e.briggs@alustudent.com>
2024-07-19 04:01:02 -0700

    symbols

---
+++ Cognitive-health.py
@@ -0,0 +1,10 @@
+ import random
+
+ # List of symbols for the cards 
+ symbols = ['🤎','🤩','🤑','🤡','😎','😂','😏','🥵']
+ symbols = symbols * 2 #to make pairs 
+
+ #to shuffle the symbols randomly 
+ random.shuffle(symbols)
+
+
