136bc4458e150e78e1f9494b59e19e8a9431b003
Emma-Asoliya <e.briggs@alustudent.com>
2024-07-20 14:52:22 -0700

    game

--- Cognitive-health.py
+++ Cognitive-health.py
@@ -7,4 +7,12 @@
  #to shuffle the symbols randomly 
  random.shuffle(symbols)
 
+#List of numbers for the cards
+numbers = ['2','4','6','8','10','12',14','16']
+numbers = numbers * 2 #to make pairs 
+random.shuffle(numbers)
 
+#List of letters for the cards 
+letters = ['A','F','H','U','L','T','C','Z']
+letters = letters * 2 #to make pairs
+random.shuffle(letters)
